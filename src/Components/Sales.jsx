7b8c7a37b643de44bb5b91878b7ee6ecb9c76d41
shubhamkohli19 <shubhamkohli172@gmail.com>
2024-01-02 17:20:41 +0530

    Improved Sales.jsx

--- src/Components/Sales.jsx
+++ src/Components/Sales.jsx
@@ -7,30 +7,30 @@ import totalSales from "../Images/totalSales.jpg";
 const Sales = () => {
   return (
     <div className="flex sales">
-      <div className="w-64 border border-black">
-        <div className="flex justify-between mx-1">
+      <div className="w-64">
+        <div className="flex justify-between mx-1 pb-6">
           <span className="font-bold">Sale</span>
           <span>
             <MoreVertIcon />
           </span>
         </div>
-        <div className="w-48 mx-auto py-4">
+        <div className="w-48 mx-auto pb-6">
           <img src={totalSales} alt="" />
         </div>
-        <div className="px-4 pb-2">
-          <div className=" flex  justify-between">
+        <div className="px-4 pb-2 text-[13px] font-semibold">
+          <div className=" flex  justify-between py-2">
             <span>Current Week</span>
             <span>2000</span>
             <span>+0.3%</span>
           </div>
-          <div className="flex justify-between">
+          <div className="flex justify-between pt-7">
             <span>Last Week</span>
             <span>1500</span>
             <span>-0.3%</span>
           </div>
         </div>
       </div>
-      <div className="w-[420px] border border-black">
+      <div className="w-[420px] border ">
         <div className="flex font-bold justify-between pl-1">
           <span>Weekly Sales Stats</span>
           <span>
@@ -40,8 +40,8 @@ const Sales = () => {
         <div className="">
           <img src={weeklySales} alt="" />
         </div>
-        <div className="flex justify-between text-[12px]">
-          <div className="h-4 w-12">
+        <div className="flex justify-between items-center py-2 text-[12px]">
+          <div className=" w-12 ">
             <img
               src="https://i.pinimg.com/originals/ae/ec/c2/aeecc22a67dac7987a80ac0724658493.jpg"
               alt=""
@@ -52,11 +52,11 @@ const Sales = () => {
             <div>UI/UX Design</div>
           </div>
           <div>
-            <div className="m-2 border border-black px-2 py-1">$20</div>
+            <div className="m-2 px-2 py-1 bg-emerald-100 text-green-800 rounded-md">$20</div>
           </div>
         </div>
-        <div className="flex justify-between text-[12px]">
-          <div className="h-4 w-12">
+        <div className="flex justify-between items-center py-2 text-[12px]">
+          <div className=" w-12">
             <img
               src="https://i.pinimg.com/originals/ae/ec/c2/aeecc22a67dac7987a80ac0724658493.jpg"
               alt=""
@@ -67,11 +67,11 @@ const Sales = () => {
             <div>UI/UX Design</div>
           </div>
           <div>
-            <div className="m-2 border border-black px-2 py-1">$20</div>
+            <div className="m-2 px-2 py-1 bg-emerald-100 text-green-800 rounded-md">$20</div>
           </div>
         </div>
-        <div className="flex justify-between text-[12px]">
-          <div className="h-4 w-12">
+        <div className="flex justify-between items-center py-2 text-[12px]">
+          <div className=" w-12">
             <img
               src="https://i.pinimg.com/originals/ae/ec/c2/aeecc22a67dac7987a80ac0724658493.jpg"
               alt=""
@@ -82,7 +82,7 @@ const Sales = () => {
             <div>UI/UX Design</div>
           </div>
           <div>
-            <div className="m-2 border border-black px-2 py-1">$20</div>
+            <div className="m-2 px-2 py-1 bg-emerald-100 text-green-800 rounded-md">$20</div>
           </div>
         </div>
       </div>
